946bb4ce598a7f602cb1e0e4efb040c12b5e18bb
Karen Amurrio <karenamurriohuaygua.com>
2026-06-19 05:01:26 -0400

    Enable Vercel SSR adapter for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mi-comiqueria.vercel.app',
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  adapter: vercel()
 });
